73b6b75896a74c25aae9bf747a5af16af5557b80
Chris Ratterree <ratt18@hotmail.com>
2021-04-30 13:58:27 -0600

    Oauth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "258096412737465",
+    clientSecret: "2aa23377c95a39932496a667ee0382fa",
+  },
 };
